1d03981ac9f39f134cbfcd17943e6f688ac3b191
libkovam-tech <libkov.a.m@gmail.com>
2026-03-04 19:27:41 +0200

    Update PixelEditorFramer.tsx
    add .tsx

--- src/editor/PixelEditorFramer.tsx
+++ src/editor/PixelEditorFramer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import { ManualScreen } from "./ManualScreen"
+import { ManualScreen } from "./ManualScreen.tsx"
 
 import {
     InlineSvgWrap,
