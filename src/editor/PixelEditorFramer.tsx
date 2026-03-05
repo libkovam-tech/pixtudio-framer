a1aa65910015544f4d52362bc45a05dc70e81338
libkovam-tech <libkov.a.m@gmail.com>
2026-03-05 11:54:55 +0200

    Update PixelEditorFramer.tsx
    color: textColor,

--- src/editor/PixelEditorFramer.tsx
+++ src/editor/PixelEditorFramer.tsx
@@ -6965,7 +6965,7 @@ function PixelEditorFramer({
         fontSize: 13,
         fontWeight: 800,
         letterSpacing: 0.4,
-        color: "blue",
+        color: textColor,
         textTransform: "uppercase",
         marginBottom: 3,
     }
